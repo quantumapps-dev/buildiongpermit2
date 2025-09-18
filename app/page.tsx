681f6b60a4b2ceb97b8a8fa0da1ff8ac8a8e391a
vivek-codepalette <155106326+vivek-codepalette@users.noreply.github.com>
2025-09-18 15:01:01 +0700

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+export default function Home() {
+
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <div className="flex h-screen">
+        {/* Main Content Area */}
+        <p> Generating your application.... It takes upto 5 minutes. Please refresh the browser if you don't see any updates in a few minutes</p>
+      </div>
+    </div>
+  );
+}
